5ee3007cccc9aa49942fd3840d52e453eb2c3d9b
antonina <grigorevna.antoninka@mail.ru>
2022-09-19 23:05:10 +0700

    домашняя

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+package ru.skyeng;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //write your code here
+        double dog = 8;
+      System.out.println(dog);
+        var cat=3.6;
+        System.out.println(cat);
+        var paper=763789;
+        System.out.println(paper);
+        dog=dog+4;
+        System.out.println(dog);
+        cat=cat+4;
+        System.out.println(cat);
+        paper=paper+4;
+        System.out.println(paper);
+        paper=paper-7639;
+        System.out.println(paper);
+        cat=cat-1.6;
+        System.out.println(cat);
+        dog=dog-3.5;
+        System.out.println(dog);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+
